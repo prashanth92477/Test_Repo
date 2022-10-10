cddf3be58bd6a26df1c4e0ea13c5c86410dbdc5a
John Doe <prashanth.qa20@gmail.com>
2022-10-09 21:14:15 -0400

    tut-assigmnet-version

--- my-app/src/app/warning-alert/warning-alert.component.ts
+++ my-app/src/app/warning-alert/warning-alert.component.ts
@@ -1,9 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { reduce } from 'rxjs';
 
 @Component({
-  selector: 'app-warning-alert',
-  templateUrl: './warning-alert.component.html',
-  styleUrls: ['./warning-alert.component.css']
+  selector: '[app-warning-alert]',
+  // templateUrl: './warning-alert.component.html',
+  template: `<p>warning-alert works!!!</p>`, // Here i am using the inline template and inline style
+  // styleUrls: ['./warning-alert.component.css']
+  styles:[`
+    /* p{
+      border-style:red;
+      padding:5px;
+      background-color:red;
+      border-radius:5px;
+    } */
+    p{
+      border-style: solid;
+      border-color: red;
+      background-color: red;
+      padding: 5px 5px 5px 5px;
+      border-radius: 5px;
+}
+  `]
 })
 export class WarningAlertComponent implements OnInit {
 
